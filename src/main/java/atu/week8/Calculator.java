7fe84e49c3ac481535c47da8939e421b9a34e300
CONORSHAUGHNESSY-STU <g00432599@atu.ie>
2025-11-17 10:26:58 +0000

    Added 'ADD METHOD' With Success & Failure Path

---
+++ src/main/java/atu/week8/Calculator.java
@@ -0,0 +1,39 @@
+package atu.week8;
+
+public class Calculator {
+    public int add(int firstNum, int secondNum)
+    {
+        if(firstNum == Integer.MAX_VALUE || secondNum == Integer.MAX_VALUE)
+        {
+            throw new ArithmeticException("Value Out Of Range");
+        }
+        return firstNum + secondNum;
+    }
+
+    public  int sub(int firstNum, int secondNum)
+    {
+        if(firstNum == Integer.MIN_VALUE || secondNum == Integer.MIN_VALUE)
+        {
+            throw new ArithmeticException("Value Out Of Range");
+        }
+        return firstNum - secondNum;
+    }
+
+    public int mul(int firstNum, int secondNum)
+    {
+        if(firstNum == Integer.MAX_VALUE || secondNum == Integer.MAX_VALUE)
+        {
+            throw new ArithmeticException("Value Out Of Range");
+        }
+        return firstNum * secondNum;
+    }
+
+    public int div(int firstNum, int secondNum)
+    {
+        if(firstNum == Integer.MIN_VALUE || secondNum == Integer.MIN_VALUE)
+        {
+            throw new ArithmeticException("Cannot Divide By Zero");
+        }
+        return firstNum / secondNum;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,31 @@
+import atu.week8.Calculator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CalculatorTest {
+    Calculator calc;
+
+    @BeforeEach
+    public void setUp()
+    {
+        calc = new Calculator();
+    }
+
+    /** ADD TEST **/
+    @Test
+    public void testAdd_Success()
+    {
+        assertEquals(20, calc.add(16,4));
+    }
+
+    @Test
+    public void testAdd_Fail()
+    {
+        Exception ex = assertThrows(ArithmeticException.class, ()-> calc.add(Integer.MAX_VALUE, 1));
+        assertEquals("Value Out Of Range", ex.getMessage());
+    }
+
+}
